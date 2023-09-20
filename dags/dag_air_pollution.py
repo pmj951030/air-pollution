1a360fe00646d16fc342e02291bbfe1ec3020df8
pmj951030 <audwls7857@naver.com>
2023-09-20 23:27:50 +0900

    emailoperator

--- dags/dag_air_pollution.py
+++ dags/dag_air_pollution.py
@@ -9,7 +9,7 @@ from airflow.operators.email import EmailOperator
 with DAG(
     dag_id="dag_air_pollution", ## airflow에들어왔을때 보이는 dag이름
     schedule="5 * * * *", ## 매일 매시간 5분에 정보 수집 {분 시 일 월 요일}
-    start_date=pendulum.datetime(2023, 9, 20, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 9, 20, tz="Asia/Seoul"), ## 서울로설정
     catchup=False ## 날짜 누락된 구간은 코드 실행x(start_date부터 어제까지의 구간은 코드실행X)
     
 
