b33ba5942eae77049a17703527df92ae482e6266
mjpark <audwls7857@gmail.com>
2023-09-24 15:08:28 +0900

    10분마다 수집으로 변경

--- dags/dag_air_pollution.py
+++ dags/dag_air_pollution.py
@@ -5,15 +5,14 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
 from common.api_crawling import get_data
 from airflow.models import Variable
-from airflow.utils.edgemodifier import Label
 
 
 var_value=Variable.get("gonggong_api_key")
 
 
 with DAG(
-    dag_id="dag_air_pollution3", ## airflow에들어왔을때 보이는 dag이름
-    schedule="5 * * * *", ## 매일 매시간 5분에 정보 수집 {분 시 일 월 요일}
+    dag_id="dag_air_pollution", ## airflow에들어왔을때 보이는 dag이름
+    schedule="*/10 * * * *", ## 매일 매시간 10분마다 정보 수집 {분 시 일 월 요일}
     start_date=pendulum.datetime(2023, 9, 20, tz="Asia/Seoul"), ## 서울로설정
     catchup=False ## 날짜 누락된 구간은 코드 실행x(start_date부터 어제까지의 구간은 코드실행X)
     
@@ -21,8 +20,6 @@ with DAG(
 ) as dag:
     ## air pollution api crawling
 
-    
-    
     air_pollution_api=PythonOperator(
         task_id='air_pollution_api',
         python_callable=get_data,
