5b30527a1f85fba9b993d8c9452bc2742dd17221
mjpark <audwls7857@gmail.com>
2023-09-24 14:39:07 +0900

    5분에 코드 실행

--- dags/dag_air_pollution.py
+++ dags/dag_air_pollution.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
 from common.api_crawling import get_data
 from airflow.models import Variable
+from airflow.utils.edgemodifier import Label
 
 
 var_value=Variable.get("gonggong_api_key")
@@ -12,7 +13,7 @@ var_value=Variable.get("gonggong_api_key")
 
 with DAG(
     dag_id="dag_air_pollution3", ## airflow에들어왔을때 보이는 dag이름
-    schedule="* * * * *", ## 매일 매시간 5분에 정보 수집 {분 시 일 월 요일}
+    schedule="5 * * * *", ## 매일 매시간 5분에 정보 수집 {분 시 일 월 요일}
     start_date=pendulum.datetime(2023, 9, 20, tz="Asia/Seoul"), ## 서울로설정
     catchup=False ## 날짜 누락된 구간은 코드 실행x(start_date부터 어제까지의 구간은 코드실행X)
     
