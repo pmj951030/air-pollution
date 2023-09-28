0f3df48ac920eb3929b005cce9322eb9ec702419
mjpark <audwls7857@gmail.com>
2023-09-28 22:17:35 +0900

    공기오염데이터 최종

--- dags/dag_air_pollution.py
+++ dags/dag_air_pollution.py
@@ -12,7 +12,7 @@ var_value=Variable.get("gonggong_api_key")
 
 with DAG(
     dag_id="dag_air_pollution", ## airflow에들어왔을때 보이는 dag이름
-    schedule="*/10 * * * *", ## 매일 매시간 10분마다 정보 수집 {분 시 일 월 요일}
+    schedule="50 * * * *", ## 매일 매시간 50분마다 정보 수집 {분 시 일 월 요일}
     start_date=pendulum.datetime(2023, 9, 20, tz="Asia/Seoul"), ## 서울로설정
     catchup=False ## 날짜 누락된 구간은 코드 실행x(start_date부터 어제까지의 구간은 코드실행X)
     
